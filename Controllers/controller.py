626f2c9592ec5986ea6895b643369bad4cb07074
arceister <imandajagad01@gmail.com>
2022-03-22 22:45:18 +0700

    add: `prediction` example

--- Controllers/controller.py
+++ Controllers/controller.py
@@ -1,2 +1,20 @@
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+
 def api_hit_response():
   return {"Message": "API Accessed!"}
+
+def predict_image():
+  model = load_model('../Models/keras_model.h5')
+  data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+  image = Image.open('../Image/apel_example.jpg')
+  size = (224, 224)
+  image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+  image_array = np.asarray(image)
+  normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+  data[0] = normalized_image_array
+
+  prediction = model.predict(data)
+  return {"Prediction": prediction}
