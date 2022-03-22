d7ccdc07c119bcc4e076c2a92803ab3fea5edaef
arceister <imandajagad01@gmail.com>
2022-03-22 16:24:23 +0700

    add: `controller`

---
+++ Controllers/controller.py
@@ -0,0 +1,2 @@
+def api_hit_response():
+  return {"Message": "API Accessed!"}
--- Routers/routes.py
+++ Routers/routes.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
+from Controllers import controller
 
 router = APIRouter()
 
 router.get("/")
 async def hit_api():
-  return {
-    "message": "Success"
-  }
+  return controller.api_hit_response()
